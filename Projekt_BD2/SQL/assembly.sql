210c781a14fb2d86203c8579c692279a026c2ca3
PanJazdan <janpazdan@student.agh.edu.pl>
2025-06-13 14:17:03 +0200

    assembly and create tables

--- Projekt_BD2/SQL/assembly.sql
+++ Projekt_BD2/SQL/assembly.sql
@@ -1,10 +1,27 @@
+drop table if exists Users;
+drop table if exists Vectors;
+drop table if exists Units;
+drop table if exists Locations;
+drop table if exists Colors;
+drop table if exists Currency;
+
 IF TYPE_ID('Email') IS NOT NULL
     DROP TYPE Email;
 IF TYPE_ID('Vector3D') IS NOT NULL
     DROP TYPE Vector3D;
+IF TYPE_ID('UnitSI') IS NOT NULL
+    DROP TYPE UnitSI;
+IF TYPE_ID('GeoLocation') IS NOT NULL
+    DROP TYPE GeoLocation;
+IF TYPE_ID('ColorRGB') IS NOT NULL
+    DROP TYPE ColorRGB;
+IF TYPE_ID('MoneyType') IS NOT NULL
+    DROP TYPE MoneyType;
 
 IF EXISTS (SELECT * FROM sys.assemblies WHERE name = 'UDTAssembly')
     DROP ASSEMBLY UDTAssembly;
+
+--ALTER DATABASE [TwojaBazaDanych] SET TRUSTWORTHY ON;
 
 CREATE ASSEMBLY UDTAssembly
 FROM 'D:\Visual Studio\Projects\Bazy danych\UserDefinedTypes\bin\Debug\UserDefinedTypes.dll'
@@ -13,7 +30,20 @@ GO
 
 CREATE TYPE Email
 EXTERNAL NAME UDTAssembly.[Email];
-GO
 
 CREATE TYPE Vector3D
 EXTERNAL NAME UDTAssembly.[Vector3D];
+
+CREATE TYPE UnitSI
+EXTERNAL NAME UDTAssembly.[UnitSI];
+
+CREATE TYPE GeoLocation
+EXTERNAL NAME UDTAssembly.[GeoLocation];
+
+CREATE TYPE ColorRGB
+EXTERNAL NAME UDTAssembly.[ColorRGB];
+
+CREATE TYPE MoneyType
+EXTERNAL NAME UDTAssembly.[MoneyType];
+
+GO
--- Projekt_BD2/SQL/table.sql
+++ Projekt_BD2/SQL/table.sql
@@ -1,16 +1,36 @@
-drop table if exists Users;
-drop table if exists Vectors;
-
-
 CREATE TABLE Users (
     Id INT IDENTITY(1,1) PRIMARY KEY, 
-    ContactEmail Email                  
-);
-GO
-
+    ContactEmail Email  NOT NULL              
+    );
 
 CREATE TABLE Vectors(
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Vec Vector3D NOT NULL
     );
+
+CREATE TABLE Units(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Ut UnitSI NOT NULL
+    );
+
+CREATE TABLE Locations(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Place VARCHAR(100) NOT NULL,
+    Location GeoLocation NOT NULL
+    );
+
+CREATE TABLE Colors (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    ColorValue ColorRGB NOT NULL
+);
+
+CREATE TABLE Currency (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    MoneyValue MoneyType NOT NULL
+);
+
+INSERT INTO Currency (MoneyValue)
+VALUES ('123.45 [PLN]');
+
+select * from Currency;
 
